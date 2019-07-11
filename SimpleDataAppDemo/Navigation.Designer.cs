9bbd43c75d190cbce51826d0f7121c68c77ff63f
Steve Bachochin <38479223+stevebachochin@users.noreply.github.com>
2019-07-11 07:22:29 -0600

    Add files via upload

---
+++ SimpleDataAppDemo/Navigation.Designer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace $safeprojectname$
+{
+    partial class Navigation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnGoToAdd = new System.Windows.Forms.Button();
+            this.btnGoToFillOrCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnInitalize = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGoToAdd
+            // 
+            this.btnGoToAdd.Location = new System.Drawing.Point(453, 87);
+            this.btnGoToAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGoToAdd.Name = "btnGoToAdd";
+            this.btnGoToAdd.Size = new System.Drawing.Size(75, 35);
+            this.btnGoToAdd.TabIndex = 1;
+            this.btnGoToAdd.Text = "Add an account";
+            this.btnGoToAdd.UseVisualStyleBackColor = true;
+            this.btnGoToAdd.Click += new System.EventHandler(this.btnGoToAdd_Click);
+            // 
+            // btnGoToFillOrCancel
+            // 
+            this.btnGoToFillOrCancel.Location = new System.Drawing.Point(453, 126);
+            this.btnGoToFillOrCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGoToFillOrCancel.Name = "btnGoToFillOrCancel";
+            this.btnGoToFillOrCancel.Size = new System.Drawing.Size(75, 35);
+            this.btnGoToFillOrCancel.TabIndex = 2;
+            this.btnGoToFillOrCancel.Text = "Fill or Cancel an order";
+            this.btnGoToFillOrCancel.UseVisualStyleBackColor = true;
+            this.btnGoToFillOrCancel.Click += new System.EventHandler(this.btnGoToFillOrCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(453, 177);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(56, 19);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(43, 12);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series7.Legend = "Legend1";
+            series7.Name = "Cows";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series8.Legend = "Legend1";
+            series8.Name = "Bulls";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series9.Legend = "Legend1";
+            series9.Name = "Calves";
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Size = new System.Drawing.Size(386, 492);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // btnInitalize
+            // 
+            this.btnInitalize.Location = new System.Drawing.Point(453, 50);
+            this.btnInitalize.Name = "btnInitalize";
+            this.btnInitalize.Size = new System.Drawing.Size(75, 23);
+            this.btnInitalize.TabIndex = 5;
+            this.btnInitalize.Text = "Refresh";
+            this.btnInitalize.UseVisualStyleBackColor = true;
+            this.btnInitalize.Click += new System.EventHandler(this.btnInitalize_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(647, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(549, 197);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Navigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 648);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnInitalize);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGoToFillOrCancel);
+            this.Controls.Add(this.btnGoToAdd);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Navigation";
+            this.Text = "Welcome";
+            this.Load += new System.EventHandler(this.FillOrCancel_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGoToAdd;
+        private System.Windows.Forms.Button btnGoToFillOrCancel;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnInitalize;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+
+
+}
+
---
+++ SimpleDataAppDemo/Navigation.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace $safeprojectname$
+{
+    public partial class Navigation : Form
+    {
+        Animals livestock;
+        List<Animals> list = new List<Animals>();
+
+
+        public Navigation()
+        {
+            InitializeComponent();
+            getLiveStock();
+            refreshGraph();
+            refreshGrid();
+        }
+
+ 
+        /**
+        private void chart1_Load(object sender, EventArgs e)
+        {
+           
+            refreshGraph();
+        }
+        **/
+
+
+        private void FillOrCancel_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// Opens the NewCustomer form as a dialog box,
+        /// which returns focus to the calling form when it is closed. 
+        /// </summary>
+        private void btnGoToAdd_Click(object sender, EventArgs e)
+        {
+            Form frm = new NewCustomer();
+            frm.Show();
+        }
+
+        /// <summary>
+        /// Opens the FillorCancel form as a dialog box. 
+        /// </summary>
+        private void btnGoToFillOrCancel_Click(object sender, EventArgs e)
+        {
+            Form frm = new FillOrCancel();
+            frm.ShowDialog();
+        }
+
+        /// <summary>
+        /// Closes the application (not just the Navigation form).
+        /// </summary>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInitalize_Click(object sender, EventArgs e)
+        {
+            refreshGraph();
+        }
+
+
+        private void refreshGraph()
+        {
+            // Convert to DataTable.
+            //DataTable table = ConvertListToDataTable(list);
+
+            DataSet ds = new DataSet();
+            ds = Common.ToDataSet(getLiveStock2());
+            //ds = getLiveStock();
+            DataTable dt = new DataTable();
+            dt = ds.Tables[0];
+
+            this.chart1.ChartAreas[0].AxisX.Title = "Breed";
+            this.chart1.ChartAreas[0].AxisY.Title = "Total Head";
+
+            this.chart1.Series.Clear();
+            this.chart1.DataBindCrossTable(dt.Rows, "Type", "Breed", "Total", "");
+
+            /**
+            this.chart1.Series["Cows"].Points.AddXY("Angus", 10);
+            this.chart1.Series["Cows"].Points.AddXY("Hereford", 3);
+            this.chart1.Series["Bulls"].Points.AddXY("Angus", 2);
+            this.chart1.Series["Bulls"].Points.AddXY("Hereford", 1);
+            this.chart1.Series["Calves"].Points.AddXY("Angus", 22);
+            this.chart1.Series["Calves"].Points.AddXY("Hereford", 61);
+    **/
+            foreach (Series cs in chart1.Series)
+            {
+                cs.ChartType = SeriesChartType.StackedColumn;
+                cs.IsValueShownAsLabel = true;
+                cs.LabelToolTip = "#VAL #SERIESNAME";
+            }
+
+
+        }
+
+        private void refreshGrid()
+        {
+            dataGridView1.DataSource = getLiveStock2();
+        }
+
+
+
+
+        private dynamic getLiveStock2()
+        {
+            using (Entities db = new Entities())
+            {
+                var query = (from cal in db.Calves select new { Breed = cal.Breed, Cull_Code = cal.Cull_Code, Type = "Calves" })
+                    .Concat(from bul in db.Bulls select new { Breed = bul.Breed, Cull_Code = bul.Cull_Code, Type = "Bulls" })
+                    .Concat(from cow in db.Cows select new { Breed = cow.Breed, Cull_Code = cow.Cull_Code, Type = "Cows" })
+                    .GroupBy(p => new { p.Breed, p.Type }, (key, group) => new { Breed = key.Breed, Type = key.Type, Total = group.Count() });
+
+                //var query2 = query.GroupBy(p => new { p.Breed, p.Type }, (key, group) => new { Breed = key.Breed, Type = key.Type, Total = group.Count() });
+
+
+                return query.ToList();
+            }
+        }
+
+        private dynamic getLiveStock()
+        {
+
+            //var query = commonClass.db.Meter_Proving.ToList();    // your starting point - table in the "from" statement
+
+            using (Entities db = new Entities())
+            {
+                List<Livestock> record = db.Livestocks.ToList();
+                return record;
+            }
+
+            /**
+            list.Add(new Animals() { Type = "Cows", Breed = "Angus", Total = 70 });
+            list.Add(new Animals() { Type = "Calves", Breed = "Angus", Total = 50 });
+            list.Add(new Animals() { Type = "Bulls", Breed = "Angus", Total = 5 });
+
+            list.Add(new Animals() { Type = "Cows", Breed = "Hereford", Total = 90 });
+            list.Add(new Animals() { Type = "Calves", Breed = "Hereford", Total = 40 });
+            list.Add(new Animals() { Type = "Bulls", Breed = "Hereford", Total = 10 });
+            **/
+        }
+
+
+    }
+
+    public class Animals
+    {
+        public string Type { get; set; }
+        public string Breed { get; set; }
+        public int Total { get; set; }
+
+        // Other properties, methods, events...
+    }
+}
